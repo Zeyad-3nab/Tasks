ddc8acc715d7bf0b8564b93323de1b4ae95d8abc
Zeyad-3nab <141088353+Zeyad-3nab@users.noreply.github.com>
2024-03-22 17:49:30 +0200

    Add files via upload

---
+++ Task1/CheckingAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class CheckingAccount:Account
+    {
+
+        public CheckingAccount(string name = "NoName", double balance = 0.0) : base(name, balance)
+        {
+
+        }
+      
+        
+        public override bool Withdraw(double amount)
+        {
+           return base.Withdraw(amount + 1.5);
+           
+        }
+
+
+    }
+}
---
+++ Task1/SavingsAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class SavingsAccount:Account
+    {
+        public double IntRate { get; set; }
+        public SavingsAccount()
+        {
+            IntRate = 3.0;
+        }
+        public SavingsAccount(string name="No Name" , double balance=0.0 , double intRate = 0.0):base(name,balance)
+        {
+            IntRate = intRate;
+        }
+        public override bool Withdraw(double amount)
+        {
+            return base.Withdraw(amount + IntRate);
+        }
+
+
+    }
+}
